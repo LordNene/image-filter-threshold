c5c687d6095aa146ccc6dc6d449189b7ee698aaf
canastro <ricardocanastro@gmail.com>
2017-01-07 16:21:09 +0000

    update image-filter-core dependency

--- tests/transform-spec.js
+++ tests/transform-spec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const transform = require('../src/threshold');
+const transform = require('../src/transform');
 
 describe('threshold', function() {
     it('should apply transformation and return as imageData', function() {
@@ -8,24 +8,15 @@ describe('threshold', function() {
             219,
             242,
             255,
-            193,
+            255,
             219,
             242,
             255
         ];
 
-        const expectedData = [
-            255,
-            255,
-            255,
-            255,
-            193,
-            219,
-            242,
-            255
-        ];
+        const expectedData = [ 0, 0, 0, 255, 255, 255, 255, 255 ];
 
-        transform(data, 4, 5);
+        transform(data, 8, { threshold: 216 });
 
         expect(data).to.deep.equal(expectedData);
     });
